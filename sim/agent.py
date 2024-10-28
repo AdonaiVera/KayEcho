c746582acb3e6db8dd3bdec1f15647d150518091
Grossman, Elan <egrossman3@gatech.edu>
2024-10-28 08:56:10 -0400

    adding agent sims

--- sim/agent.py
+++ sim/agent.py
@@ -8,18 +8,21 @@ from langchain.docstore.document import Document
 from langchain_core.runnables import RunnablePassthrough
 from langchain_core.output_parsers import StrOutputParser
 
+import requests
+import json
+
 
 
 
 class Agent():
-    def __init__(self, name, info, backstory, llm):
+    def __init__(self, name, llm, profile = None, seed=False):
 
 
         self.name = name
         self.llm = llm.llm
-        self.backstory = ",".join(backstory)
+        # self.backstory = ",".join(backstory)
         self.text_splitter = llm.text_splitter
-        self.info = info
+        # self.info = info
 
         # chunks = self.get_chunks_self(backstory)
         # documents = []
@@ -38,19 +41,21 @@ class Agent():
         #             "id":i
         #         }
         # ))
-        backstory.append("My name is {name}".format(name=self.name))
-
-        documents = self.text_splitter.create_documents(backstory)
-
         
-        self.counter = 0
-        # chunks = backstory
+        direct = "memories/"+self.name.lower().strip().replace(" ","_")
 
-        self.db = Chroma.from_documents(
-        documents,
-        llm.embeddings,
-        persist_directory="memories/"+self.name.lower().strip().replace(" ","_")
-    )
+        if seed:
+            backstory = self.parse_profile_data(profile)
+            backstory.append("My name is {name}".format(name=self.name))
+            documents = self.text_splitter.create_documents(backstory)
+            # chunks = backstory
+            self.db = Chroma.from_documents(
+            documents,
+            llm.embeddings,
+            persist_directory=direct
+        )
+        else:
+            self.db=Chroma(persist_directory=direct, embedding_function=llm.embeddings)
         
         self.retriever = self.db.as_retriever()
 
@@ -141,14 +146,108 @@ class Agent():
     def retrieve_memory(self, query):
         return self.qa(query)
 
-    def save_memory(self, text, agent):
+    def save_memory(self, text):
         # doc = Document(page_content=text,
         #                metadata={"source": "conversation", "agent":agent, "id": self.counter})
         
         doc = self.text_splitter.create_documents([text])
-        self.db.add_documents(doc,ids=[str(self.counter)])
-        self.counter+=1
+        self.db.add_documents(doc)
+        # self.counter+=1
 
+    def parse_profile_data(self, file_location):
+
+        with open(file_location, "r") as f:
+            data = json.load(f)
+
+        data = data['response']['response']
+
+        collection = []
+        for k in data.keys():
+            if k not in ['gender','linkedin','email','follower_count','premium']:
+                statements = self.get_profile_info(data[k],k)
+                if len(statements) > 0:
+                    for stat in statements:
+                        collection.append(stat)
+
+        return collection
+
+
+
+    def get_profile_info(self,field,field_name):
+
+        prompt = """
+        You are an expert data analyst who is given a json that represents data on an individual
+
+        Field Name: {field}
+
+        Data = {data}
+
+        -------
+        Task: Given the Field Name and the data, produce a series of statements in first person.
+
+        For example:
+        - I live in New York
+        - I work at Google
+        - I went to School at NYU
+
+        Output Format:
+        A python list of results:
+
+        ["I Live in New York", "I work at Google", "I went to school at NYU"]
+
+        
+        
+        """.format(field = field_name, data = json.dumps(field))
+
+        fields = self.llm.invoke(prompt).content
+        return fields.split(",")
+    
+
+    def get_recent_memory(self, n=10):
+        return self.db.get()["documents"][-n:]
+    
+    def generate_plans(self):
+        memory = self.get_recent_memory(20)
+
+        prompt = """Given these memories of previous events, create a plan for what you would like to learn
+
+        Memories: {memories}
+
+
+        Output: A list of things to learn
+        """.format(memories = ",".join(memory))
+        
+        return self.llm.invoke(prompt)
+        
+
+
+def test_get_profile(profile_linkedin_1):
+    url= 'https://kayecho-364607428894.us-central1.run.app'
+    url_profile_linkedin = f'{url}/userProfile'
+    payload_profile = {
+        "linkedin_id": profile_linkedin_1
+    }
+    response = requests.post(url_profile_linkedin, json=payload_profile)
+    if response.status_code == 200:
+        print("Response from userProfile:", response.json())
+        return response.json()
+    else:
+        print("Error:", response.status_code, response.text)
+
+
+    
+def download_data(urls):
+
+    for u in urls:
+        json_data = test_get_profile(u)
+        name = json_data['response']['response']['full_name']
+        file_name = "profiles/"+ name.lower().replace(" ","_").strip() + ".json"
+
+        json_data['response']['response']['linkedin'] = None
+        json_data['response']['response']['email'] = None
+
+        with open(file_name, 'w', encoding='utf-8') as f:
+            json.dump(json_data, f, ensure_ascii=False, indent=4)
 
 if __name__ == "__main__":
 
@@ -162,16 +261,61 @@ if __name__ == "__main__":
     # Elan Receivedd his bachelors from Georgia Institute of Technology in Physics.
     # """
 
-    backstory = [
-        "I live in from Atlanta, Georgia",
-    "I habe worked at Deloitte for over 2 years as a senior AI consultant.",
-    "I help clients solve problems using AI\n- NLU intent Detection\n- Causal ML\n- Generative AI - GANs and LLMs\n- Obligatory PowerPoint slides.",
-    "I Previously worked at The Home Depot as an innovation engineer where his responsibilities were: Perform Applied Research and Development\n- Work with stakeholders across the business to determine new revenue streams or solve existing - Inefficiencies through modern advances in technology\n- Quickly prototype new technology for retail such as AR, VR, AI/ML, and Robotics\n- Use expertise in Machine Learning and Software Development to architect novel applications from the ground up\n- NLP, Computer Vision, Linear Optimization, Game Development, Time Series Predictions\nMentor and manage developers and interns to complete MVPs and research\n- Organize and archive results from research to hand off to any interested parties\n- Work closely with UX and product management to conduct research and interviews for new products",
-    "I Recieved my Masters from the Georgia Institute of Technology in Computer Science.",
-    "I Receivedd my bachelors from Georgia Institute of Technology in Physics."
-    ]
+    # backstory = [
+    #     "I live in from Atlanta, Georgia",
+    # "I habe worked at Deloitte for over 2 years as a senior AI consultant.",
+    # "I help clients solve problems using AI\n- NLU intent Detection\n- Causal ML\n- Generative AI - GANs and LLMs\n- Obligatory PowerPoint slides.",
+    # "I Previously worked at The Home Depot as an innovation engineer where his responsibilities were: Perform Applied Research and Development\n- Work with stakeholders across the business to determine new revenue streams or solve existing - Inefficiencies through modern advances in technology\n- Quickly prototype new technology for retail such as AR, VR, AI/ML, and Robotics\n- Use expertise in Machine Learning and Software Development to architect novel applications from the ground up\n- NLP, Computer Vision, Linear Optimization, Game Development, Time Series Predictions\nMentor and manage developers and interns to complete MVPs and research\n- Organize and archive results from research to hand off to any interested parties\n- Work closely with UX and product management to conduct research and interviews for new products",
+    # "I Recieved my Masters from the Georgia Institute of Technology in Computer Science.",
+    # "I Receivedd my bachelors from Georgia Institute of Technology in Physics."
+    # ]
 
-    elan = Agent("Elan Grossman", backstory, llm)
-    print(elan.retrieve_memory("where do you live?"))
+    # elan_info = "An expeirenced data scientist with expertise in Computer Vision, NLP, and LLMs"
 
+    # elan = Agent("Elan Grossman", llm, profile = "profiles/elan_grossman.json", seed=True)
+    # print(elan.retrieve_memory("where do you live?"))
+    # urls = [
+    #     "https://www.linkedin.com/in/arnav-kumar-66419027a/",
+    #     "https://www.linkedin.com/in/jonathan-groberg/",
+    #     "https://www.linkedin.com/in/adonai-vera/",
+    #     "https://www.linkedin.com/in/luisesilvavargas/",
+    #     "https://linkedin.com/in/rishabkalluri/",
+    #     "https://www.linkedin.com/in/shreya-mahesh001/",
+    #     "https://www.linkedin.com/in/shraya-patel/",
+    #     "https://www.linkedin.com/in/adityagupta001/",
+    #     "https://www.linkedin.com/in/wyatt-bunch/",
+    #     "https://www.linkedin.com/in/elan-grossman/"
+
+    # ]
+    urls = ["https://www.linkedin.com/in/dynamicwebpaige/",
+            "https://www.linkedin.com/in/alex-albert/",
+            "https://www.linkedin.com/in/zaiddoski/",
+            "https://www.linkedin.com/in/nathaliaalicea/",
+            "https://www.linkedin.com/in/irfanessa/",
+            "https://www.linkedin.com/in/kem-ezeoke-149b1a53/",
+            "https://www.linkedin.com/in/vaishnavitumsi/",
+            "https://www.linkedin.com/in/aaronbegg1/"]
+
+    # download_data(urls)
+
+
+    # elan = Agent("Elan Grossman", llm, profile = "profiles/elan_grossman.json", seed=True)
+    # adonai = Agent("Adonai Vera", llm, profile = "profiles/adonai_vera.json", seed=True)
+    # aditya = Agent("Aditya Gupta", llm, profile = "profiles/aditya_gupta.json", seed=True)
+    # arnav = Agent("Arnav Kumar", llm, profile = "profiles/arnav_kumar.json", seed=True)
+    # jonathan = Agent("Jonathan Groberg", llm, profile = "profiles/jonathan_groberg.json", seed=True)
+    # luis = Agent("Luis Silva", llm, profile = "profiles/luis_s.json", seed=True)
+    # rishab = Agent("Rishab Kalluri", llm, profile = "profiles/rishab_kalluri.json", seed=True)
+    # shraya = Agent("Shraya Patel", llm, profile = "profiles/shraya_patel.json", seed=True)
+    # shreya = Agent("Shreya Mahesh", llm, profile = "profiles/shreya_mahesh.json", seed=True)
+    # shreya = Agent("Wyatt Bunch", llm, profile = "profiles/wyatt_bunch.json", seed=True)
+
+    # paige = Agent("Paige Bailey", llm, profile = "profiles/paige_bailey.json", seed=True)
+    # irfan = Agent("Irfan Essa", llm, profile = "profiles/irfan_essa.json", seed=True)
+    # alex = Agent("Alex Albert", llm, profile = "profiles/alex_albert.json", seed=True)
+    # kem = Agent("Kem Ezeoke", llm, profile = "profiles/kem_ezeoke.json", seed=True)
+    nathalia = Agent("Nathalia Alicea", llm, profile = "profiles/nathalia_alicea.json", seed=True)
+    zaid = Agent("Zaid Doski", llm, profile = "profiles/zaid_doski.json", seed=True)
+    vaishnavi = Agent("Vaishnavi Tumsi", llm, profile = "profiles/vaishnavi_tumsi.json", seed=True)
+    aaron = Agent("Aaron Begg", llm, profile = "profiles/aaron_begg.json", seed=True)
 
--- sim/sim.py
+++ sim/sim.py
@@ -1,5 +1,7 @@
 from llm import LLM
 from agent import Agent
+import numpy as np
+from langchain.docstore.document import Document
 
 
 class Convo():
@@ -19,7 +21,7 @@ class Convo():
         return prompt.strip()
 
 
-    def converse(self,agent1, agent2, N=8):
+    def converse(self,agent1, agent2, N=4):
 
         curr_chat = []
 
@@ -63,7 +65,15 @@ class Convo():
             utterance = self.generate_utterance(agent2,agent1.name, relevant_context, relationship, last_chat, answer_context)
             utterance = utterance.split(":")[-1].replace("}","")
             curr_chat += [[agent2.name, utterance]]
+
             print(curr_chat[-1])
+        memory = self.get_memo(agent1,agent2.name,curr_chat)
+
+        agent1.save_memory(memory)
+
+        memory = self.get_memo(agent2,agent1.name,curr_chat)
+
+        agent2.save_memory(memory)
 
     def generate_utterance(self, init_agent, target_agent_name, context, relationship, chat, answer_context):
 
@@ -92,14 +102,9 @@ class Convo():
                 Based on the chat history above, along with the Statements and relationship known about !<INPUT 2>! by !<INPUT 1>!, how should !<INPUT 1>! answer !<INPUT 2>!'
 
                 Step 1:
-                    Print '####'
+                If chat history is present, generate an answer to the last question in the Chat History using the context, otherwise, ask them about what they do for thier job. Do not repeat any precious questions
+
                 Step 2:
-                If chat history is present, generate an answer to the last question in the Chat History using the context, otherwise, ask them about what they do for thier job
-
-                Step 3:
-                Print '####'
-
-                Step 4:
                 Ask a question from !<INPUT 1>! to !<INPUT 2>! from the perspective of!<INPUT 1>!
 
                 Output format: Output a json of the following format: 
@@ -136,7 +141,7 @@ class Convo():
         if len(new_context) > 0:
             relevant_context = new_context[0:N]
         else:
-            relevant_context = [relevant_context[0]]
+            relevant_context = [(Document(page_content="I don't know about {target_person_name}".format(target_person_name=target_person_name)),0)]
 
         return relevant_context
     
@@ -165,6 +170,32 @@ class Convo():
                 
         
         return self.llm.invoke(new_prompt), relevant_context
+    
+    def get_memo(self, init_person, target_person_name, statements):
+
+
+        prompt_template = """Variables: 
+                !<INPUT 0>! -- Statements
+                !<INPUT 1>! -- curr persona name
+                !<INPUT 2>! -- target_persona.scratch.name
+
+                <commentblockmarker>###</commentblockmarker>
+                [Statements]
+                !<INPUT 0>!
+
+                Based on the statements above, summarize the conversation in 4 sentences or less from !<INPUT 1>! 's perspective about !<INPUT 2>! in first person using I statements as if you were !<INPUT 1>!"""
+        
+        variables = [
+            statements,
+            init_person.name,
+            target_person_name,
+
+        ]
+
+        new_prompt = self.generate_prompt(prompt_template, variables)
+                
+        
+        return self.llm.invoke(new_prompt)
 
     def converse_old(self, agent1, agent2, N=5):
 
@@ -271,30 +302,65 @@ if __name__ == "__main__":
 
     llm = LLM()
 
-    backstory_elan = [
-    "I live in from Atlanta, Georgia",
-    "I have worked at Deloitte for over 2 years as a senior AI consultant at Deloitte.",
-    "I help clients solve problems using AI\n- NLU intent Detection\n- Causal ML\n- Generative AI - GANs and LLMs\n- Obligatory PowerPoint slides.",
-    "I Previously worked at The Home Depot as an innovation engineer where his responsibilities were: Perform Applied Research and Development\n- Work with stakeholders across the business to determine new revenue streams or solve existing - Inefficiencies through modern advances in technology\n- Quickly prototype new technology for retail such as AR, VR, AI/ML, and Robotics\n- Use expertise in Machine Learning and Software Development to architect novel applications from the ground up\n- NLP, Computer Vision, Linear Optimization, Game Development, Time Series Predictions\nMentor and manage developers and interns to complete MVPs and research\n- Organize and archive results from research to hand off to any interested parties\n- Work closely with UX and product management to conduct research and interviews for new products",
-    "I Recieved my Masters from the Georgia Institute of Technology in Computer Science.",
-    "I Received my bachelors from Georgia Institute of Technology in Physics."
-    ]
+    # backstory_elan = [
+    # "I live in from Atlanta, Georgia",
+    # "I have worked at Deloitte for over 2 years as a senior AI consultant at Deloitte.",
+    # "I help clients solve problems using AI\n- NLU intent Detection\n- Causal ML\n- Generative AI - GANs and LLMs\n- Obligatory PowerPoint slides.",
+    # "I Previously worked at The Home Depot as an innovation engineer where his responsibilities were: Perform Applied Research and Development\n- Work with stakeholders across the business to determine new revenue streams or solve existing - Inefficiencies through modern advances in technology\n- Quickly prototype new technology for retail such as AR, VR, AI/ML, and Robotics\n- Use expertise in Machine Learning and Software Development to architect novel applications from the ground up\n- NLP, Computer Vision, Linear Optimization, Game Development, Time Series Predictions\nMentor and manage developers and interns to complete MVPs and research\n- Organize and archive results from research to hand off to any interested parties\n- Work closely with UX and product management to conduct research and interviews for new products",
+    # "I Recieved my Masters from the Georgia Institute of Technology in Computer Science.",
+    # "I Received my bachelors from Georgia Institute of Technology in Physics."
+    # ]
 
-    elan_info = "An expeirenced data scientist with expertise in Computer Vision, NLP, and LLMs"
+    # elan_info = "An expeirenced data scientist with expertise in Computer Vision, NLP, and LLMs"
 
-    elan = Agent("Elan Grossman", elan_info, backstory_elan, llm)
+    # elan = Agent("Elan Grossman", elan_info, backstory_elan, llm)
 
-    backstory_ado = [
-    "I live in Cincinnati, Ohio",
-    "I work as a machine learning developer at SubterraAI",
-    "I specialise in underground mapping and digitisation of underground infrastructure using photogrammetry, SLAM and AI to build digital twins.",
-    "I used to work at as Cofounder and CTO at SwitchAI",
-    "I worked to understand the behavior of your customers in the store and help the retail sector to enhance the impact of its strategies through variables such as gender, age, heat zones, among others, thanks to the power of intelligence artificial in your security cameras."
-    ]
+    # backstory_ado = [
+    # "I live in Cincinnati, Ohio",
+    # "I work as a machine learning developer at SubterraAI",
+    # "I specialise in underground mapping and digitisation of underground infrastructure using photogrammetry, SLAM and AI to build digital twins.",
+    # "I used to work at as Cofounder and CTO at SwitchAI",
+    # "I worked to understand the behavior of your customers in the store and help the retail sector to enhance the impact of its strategies through variables such as gender, age, heat zones, among others, thanks to the power of intelligence artificial in your security cameras."
+    # ]
 
-    ado_info = "A Machine Learning Engineer with skills in Computer Vision and Digital twins"
+    # ado_info = "A Machine Learning Engineer with skills in Computer Vision and Digital twins"
 
-    ado = Agent("Ado Vera", ado_info ,backstory_ado, llm)
+    # ado = Agent("Ado Vera", ado_info ,backstory_ado, llm)
 
     convo = Convo(llm)
-    convo.converse(elan,ado)
+
+    elan = Agent("Elan Grossman", llm)
+    adonai = Agent("Adonai Vera", llm)
+    aditya = Agent("Aditya Gupta", llm)
+    arnav = Agent("Arnav Kumar", llm)
+    jonathan = Agent("Jonathan Groberg", llm)
+    luis = Agent("Luis Silva", llm)
+    rishab = Agent("Rishab Kalluri", llm)
+    shraya = Agent("Shraya Patel", llm)
+    shreya = Agent("Shreya Mahesh", llm)
+    wyatt = Agent("Wyatt Bunch", llm)
+
+    paige = Agent("Paige Bailey", llm)
+    irfan = Agent("Irfan Essa", llm)
+    alex = Agent("Alex Albert", llm)
+    kem = Agent("Kem Ezeoke", llm)
+    nathalia = Agent("Nathalia Alicia",llm)
+    zaid = Agent("Zaid Doski", llm)
+    vaishnavi = Agent("Vaishnavi Tumsi",llm)
+    aaron = Agent("Aaron Begg", llm)
+
+    agents = np.array([elan,adonai,arnav,aditya,jonathan,luis,rishab,shraya,shreya,wyatt, paige,irfan,alex,kem,nathalia, zaid, vaishnavi, aaron])
+
+    np.random.shuffle(agents)
+
+
+
+    for i in range(6):
+        np.random.shuffle(agents)
+        print(agents)
+        print(agents[0])
+        for k in range(5):
+            agent1 = agents[k]
+            agent2 = agents[k+1]
+
+            convo.converse(agent1,agent2, 4)
